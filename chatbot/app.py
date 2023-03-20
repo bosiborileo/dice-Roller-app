586d7c97df4ff67e02b7c56141e1e611a7a82e77
bosibori leo <annetesamson@gmail.com>
2023-03-20 19:41:10 +0300

    UI

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,64 +1,90 @@
-import os
-import tensorflow as tf
-
-
+import nltk
+import json
+import warnings
 import random
-import tkinter as tk
-from tkinter import *
-from chat import get_response
+import numpy as np
+import pickle
+import chatbot
+import requests
+import os
+import time
 
-root = tk.Tk()
-root.title(f"Chatbot")
-root.geometry('600x500' )
-root.resizable(True, True)
-message = tk.StringVar()
+from chatbot import app
+from flask import Flask
+from flask import render_template,flash, request
 
-chat_win=Frame(root, bd=1, bg='black', width= 70, height=4)
-chat_win.place(x=6, y=6, height=300, width=488)
+warnings.filterwarnings('ignore')
 
-textcon= tk.Text(chat_win, bd=1, bg='white', width=50, height=8)
-textcon.pack(fill="both", expand=True)
+from nltk.stem import WordNetLemmatizer
+from keras.models import load_model
 
-mes_win= Entry(root, width=30, xscrollcommand=True, textvariable=message)
-mes_win.place(x=6, y=310, height=90, width=450)
-mes_win.focus()
+lemmatizer = WordNetLemmatizer()
 
-textcon.config(fg='black')
-textcon.tag_config('usr', foreground='Black')
-textcon.insert(END, "Bot: CHUKA BOT! Your University Assistant. \n\n")
-msg=mes_win.get()
+intents = json.loads(open('intents.json').read())
 
-exit_list = ["Thanks", "Thank you", "That's helpful"]
+with open('words.pkl', 'rb') as f:
+     words = pickle.loads(f.read())
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model')
 
-def greet_res(text):
-    text=text.lower()
-    bot_greet= ["hello", "hi", "good day", "greetings", "how is it going?"]
-    usr_greet= ["hello", "hi", "good day", "greetings", "how is it going?"]
-    for word in text.split():
-        if word in usr_greet:
-            return random.choice(bot_greet)
+def clean_up_sentences(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word)
+                      for word in sentence_words]
+    return sentence_words
+  
+def bagofwords(sentence):
+    sentence_words = clean_up_sentences(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+  
+def predict_class(sentence):
+    bow = bagofwords(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res)
+               if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]],
+                            'probability': str(r[1])})
+        return return_list
+  
+def get_response(intents_list, intents_json):
+    #print tag
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    result = ""
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
 
-def send_msz(event=None):
-    usr_input = message.get()
-    usr_input = usr_input.lower()
-    textcon.insert(END, f'You: {usr_input}'+'\n', 'usr')
-    if usr_input in exit_list:
-        textcon.config(fg='black')
-        textcon.insert(END, "Bot: Ok bye! Chat with you later \n")
-        return root.destroy()
-    else:
-        textcon.config(fg='black')
-        if greet_res(usr_input) != None:
-            lab= f"Bot: {greet_res(usr_input)}"+ '\n'
-            textcon.insert(END,lab)
-            mes_win.delete(0,END)
-        else:
-            lab= f"Bot: {get_response(usr_input)}"+'\n'
-            textcon.insert(END,lab)
-            mes_win.delete(0,END)
+def response(text):
+    return_list=predict_class(text,model)
+    response,_=get_response(return_list,intents,text)
+    return response
+  
+print("Let's chat! (type 'quit' to exit)")
+  
+@app.route('/',methods=['GET','POST'])
+#@app.route('/home',methods=['GET','POST'])
+def yo():
+    return render_template('main.html')
 
-button_send=Button(root,text='Send', bg='orange', activebackground='white', command=send_msz, width=12, height=5, font=('Arial'))
-button_send.place(x=376, y=310, height=60, width=110)
-root.bind('<Return>', send_msz, button_send)
-root.mainloop()
+@app.route('/chat',methods=['GET','POST'])
+#@app.route('/home',methods=['GET','POST'])
+def home():
+    return render_template('index.html')
 
+@app.route("/get")
+def chatbot():
+    userText = request.args.get('msg')
+    resp=response(userText)
+    return resp
