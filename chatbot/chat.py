6960a9dcd7e1227cd76353adaf5e69c0bae19c62
bosibori leo <annetesamson@gmail.com>
2023-04-02 21:44:27 +0300

    ui

--- chatbot/chat.py
+++ chatbot/chat.py
@@ -40,73 +40,73 @@ classes = pickle.load(open('classes.pkl', 'rb'))
 from keras.models import load_model
 model = load_model('chatbot_model')'''
 
-def clean_up_sentence(sentence):
+def clean_text(text):
     # tokenize the pattern - split words into array
-    sentence_words = nltk.word_tokenize(sentence)
+    tokens = nltk.word_tokenize(text)
     # stem each word - create short form for word
-    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
-    return sentence_words
+    tokens = [lemmatizer.lemmatize(word.lower()) for word in tokens]
+    return tokens
 # return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
 
-def bow(sentence, words, show_details=True):
+def bag_of_words(text, vocab):
     # tokenize the pattern
-    sentence_words = clean_up_sentence(sentence)
+    tokens = clean_text(text)
     # bag of words - matrix of N words, vocabulary matrix
-    bag = [0]*len(words) 
-    for s in sentence_words:
-        for i,w in enumerate(words):
-            if w == s: 
+    bow = [0]*len(words) 
+    for w in tokens:
+        for idx, word in enumerate(vocab):
+            if word == w: 
                 # assign 1 if current word is in the vocabulary position
-                bag[i] = 1
-                if show_details:
-                    print ("found in bag: %s" % w)
-    return(np.array(bag))
+                bow[idx] = 1
+                
+    return(np.array(bow))
 
-def predict_class(sentence, model):
+def predict_class(text, vocab, labels):
     # filter out predictions below a threshold
-    p = bow(sentence, words,show_details=False)
-    res = model.predict(np.array([p]))[0]
+    bow = (text, vocab)
+    result = model.predict(np.array([bow]))[0]
     ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
-    # sort by strength of probability
-    results.sort(key=lambda x: x[1], reverse=True)
+    y_pred = [[idx, res] for idx, res in enumerate(result) if res > ERROR_THRESHOLD]
+    y_pred.sort(key=lambda x: x[1], reverse=True)
     return_list = []
-    for r in results:
-        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    for r in y_pred:
+       return_list.append(labels[r[0]])
     return return_list
 
-def getResponse(ints, intents_json):
-    tag = ints[0]['intent']
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if(i['tag']== tag):
-            result = random.choice(i['responses'])
-            break
-    return result
+def get_response(intents_list, intents_json): 
+    tag = intents_list[0]
+    list_of_intents = intents_json["intents"]
+    for i in list_of_intents: 
+        result= random.choice(i['responses'])
+    return result,tag
 
 def chatbot_response(text):
-    ints = predict_class(text, model)
-    res = getResponse(ints, intents)
-    return res
-
+    message = input("")
+    ints = predict_class(message, words, classes)
+    response = get_response(intents, ints)
+    return response
 
 from flask import Flask, render_template, request
-app = Flask(__name__, template_folder='templates')
-@app.route('/')
+app = Flask(__name__, template_folder='template')
+
+@app.route('/',methods=['GET','POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/home',methods=['GET','POST'])
+def register():
+    return render_template('register.html')
+
+@app.route('/chat',methods=['GET','POST'])
 def index():
     return render_template('index.html')
 
 @app.route('/get')
-def get_response():
+@app.route("/get")
+def chatbot():
     userText = request.args.get('msg')
-    if get_response(userText).confidence < 0.5:
-        return str("Sorry, I don't understand")
-    else:
-        get_response(userText).confidence = 1
-        return str(get_response(userText))
+    resp=chatbot_response(userText)
+    return resp
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-           
+    app.run(debug=True)        
 
--- chatbot/training.py
+++ chatbot/training.py
@@ -3,6 +3,7 @@ import pickle
 import numpy as np
 import nltk
 import random
+import string
 
 nltk.download('punkt')#sentence tokenizer
 nltk.download('wordnet') #lexical database for the English 
@@ -27,22 +28,28 @@ with open('chatbot\intents.json', 'r') as intents:
 for intent in data['intents']:
     for pattern in intent['patterns']:
         #tokenize each word
-        w = nltk.word_tokenize(pattern)
-        words.extend(w)# add each elements into list
+        tokens = nltk.word_tokenize(pattern)
+        words.extend(tokens)# add each elements into list
         #combination between patterns and intents
-        documents.append((w, intent['tag']))# add single element into end of list
+        # add single element into end of list
+        documents.append(pattern)
+        ignore_words.append(intent["tag"])
         # add to tag in our classes list
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
 # lemmatize, lower each word and remove duplicates
-words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in string.punctuation]
 words = sorted(list(set(words)))
+classes = sorted(list(set(classes)))
 
 #print words
 print (len(words), "unique lemmatized words\n", words, "\n")
 
 #print classes
+print (len(classes), "classes\n", classes, "\n")
+
+#words = all words, vocabulary
 pickle.dump(words,open('words.pkl','wb'))
 pickle.dump(classes,open('classes.pkl','wb'))
 
@@ -50,26 +57,34 @@ pickle.dump(classes,open('classes.pkl','wb'))
 training = []
 output_empty = [0]*len(classes)
 
-for document in documents:
-    bag = []
-    word_patterns = document[0]
-    word_patterns = [lemmatizer.lemmatize(
-        word.lower()) for word in word_patterns]
+# creating the bag of words model
+for idx, doc in enumerate(documents):
+    bow = []
+    text = lemmatizer.lemmatize(doc.lower())
     for word in words:
-        bag.append(1) if word in word_patterns else bag.append(0)
-          
-    # making a copy of the output_empty
+        if word in text:
+          bow.append(1)
+        else:
+            bow.append(0)
+    # mark the index of class that the current pattern is associated
     output_row = list(output_empty)
-    output_row[classes.index(document[1])] = 1
-    training.append([bag, output_row])
+    output_row[classes.index(classes[1])] = 1
+    # add BoW and associated classes to training 
+    training.append([bow, output_row])
 
 random.shuffle(training)
-training= np.array(training)
+training= np.array(training, dtype=object)
   
 # splitting the data
-train_x = list(training[:, 0])
-train_y = list(training[:, 1])
+train_x = np.array(list(training[:, 0]))
+train_y = np.array(list(training[:, 1]))
 
+#defining parameters
+input_shape = (len(train_x[0]),)
+output_shape = len(train_y[0])
+epochs = 500
+
+#deep learning model
 model = Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
@@ -82,6 +97,7 @@ model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy
 
 history = model.fit(np.array(train_x), np.array(train_y), epochs=500, batch_size=5)
 model.save('model.h5', history)
+print('model summary')
 print("Training data has been created")
 
 
